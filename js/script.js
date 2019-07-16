39cc7882db55876c95525c132bc43ddaf179f54f
Vincent <vince3330@gmail.com>
2019-07-16 13:42:50 -0700

    Todo Project Revamp
    Redid the Todo project to Kelvin's specs.

--- js/script.js
+++ js/script.js
@@ -1,229 +1,257 @@
-// //Create a close button for each list
-// var myNodeList = document.getElementsByTagName("LI");
-// for(i = 0; i < myNodeList.length; i++){
-// 	var span = document.createElement("SPAN");
-// 	var txt = document.createTextNode("\u00D7");
-// 	span.className = "close";
-// 	span.appendChild(txt);
-// 	myNodeList[i].appendChild(span);
-// }
+//Array that stores the todo tasks
+var todoTasks = [];
+//Array that stores all the active tasks
+var activeTasks = [];
+//Array that stores all the completed tasks
+var completedTasks = [];
+//Stores the div element that houses the UL and its LI elements
+var todoContainer = document.getElementById("divContainer");
+//Stores the textbox object
+var inputByEnter = document.getElementById("todoInput");
+//Store the submit button object
+var inputBySubmit = document.getElementById("submitButton");
+//Store the button that shows all tasks
+var allButton = document.getElementById("allButton");
+//Store the button that shows all active tasks
+var activeButton = document.getElementById("activeButton");
+//Store the button that shows all completed tasks
+var doneButton = document.getElementById("doneButton");
+//Store the current view of the webpage (All/Active/Completed)
+var currView = 'ALL';
 
 //When user inputs text into textbox, clicking enter will submit it
-var inputByEnter = document.getElementById("textbox");
-
 inputByEnter.addEventListener("keyup", function(event){
-	//13 signifies the Enter key
-	if(event.keyCode === 13){	
-		//This cancels the default action if necessary
-		event.preventDefault();
-		//Call the addItem() function below
-		addItem();
-	}
+  //Get the text that the user inputted
+  var inputItem = inputByEnter.value;
+
+  //13 signifies the Enter key
+  if(event.keyCode === 13){	
+    //This cancels the default action if necessary
+    event.preventDefault();
+        //Check if there is an input
+    if(inputItem === ''){
+      alert("Please enter a to-do task!");
+    }
+
+    else{
+      addTask(inputItem);
+      inputByEnter.value = "";
+    }
+  }
 });
 
+//When user inputs text into textbox, clicking the submit button will submit it
+inputBySubmit.addEventListener("click", function(event){
+  //Get the text that the user inputted
+  var inputItem = inputByEnter.value;
 
-//Create a new item on the list
-function addItem() {
+  if(inputItem === ''){
+    alert("Please enter a to-do task!");
+  }
 
-	//Create new list element
-	var li = document.createElement("li");
+  else{
+    addTask(inputItem);
+    inputByEnter.value = "";
+  }
+});
 
-	//Set a 'contentEditable' attribute to keep track of whether checkbox is checked
-	li.setAttribute('contentEditable', 'false');
+//call the showAll method when the showAll button is clicked
+allButton.addEventListener("click", function(event){
+  showAll();
+});
 
-	//Create a checkbox
-	var checkbox = document.createElement("INPUT");
-	checkbox.setAttribute("type", "checkbox");
-	// checkbox.setAttribute("onclick", "toggleChecked();") //no longer needed
-	checkbox.className += ' listcheckbox';
-	li.appendChild(checkbox);
+//call the showActive method when the showActive button is clicked
+activeButton.addEventListener("click", function(event){
+  showActive();
+});
 
-	//Fetch the user input data
-	var inputItem = document.getElementById("textbox").value;
+//call the showDone method when the showDone button is clicked
+doneButton.addEventListener("click", function(event){
+  showDone();
+});
 
-	//Create a text node with the input data
-	// var newNode = document.createTextNode(inputItem);
-	// li.appendChild(newNode);
+//Add a an input task
+function addTask(inputTask){
+  const todo = {
+    inputTask,
+    checked: false,
+    id: Date.now()
+  };
 
-	//Check if there is an input
-	if(inputItem === ''){
-		alert("Please enter a to-do task!");
-	}
+  todoTasks.push(todo);
+  console.log(todoTasks);
 
-	else{
-		document.getElementById("list").appendChild(li);
-	}
-
-	//TRY LABEL
-	var span3 = document.createElement("LABEL");
-	var txt3 = document.createTextNode(inputItem);
-	span3.appendChild(txt3);
-	span3.className = "listdata";
-	li.appendChild(span3);
-	//TRY LABEL
-
-	//TRY THIS - Contain in SPAN
-	// var span2 = document.createElement("SPAN");
-	// var txt2 = document.createTextNode(inputItem);
-	// span2.appendChild(txt2);
-	// span2.className = "listdata";
-	// li.appendChild(span2);
-	//TRY THIS
-
-	document.getElementById("textbox").value = "";
-
-	var span = document.createElement("SPAN");
-
-	var txt = document.createTextNode(" \u00D7");
-
-	span.className = "close";
-
-	span.appendChild(txt);
-
-	li.appendChild(span);
-
-	for(i = 0; i < close.length; i++){
-		close[i].onclick = function(){
-			var div = this.parentElement;
-			div.style.display = "none";
-		}
-	}
+  rerender(todoTasks);
 }
 
+// //Removes a task from the array when its close button is clicked
+function obliterateTask(index, id){
+  if(currView === 'ACTIVE'){
+    const currId = id;
+    activeTasks.splice(index, 1);
+    rerender(activeTasks);
 
+    //Look for the task to delete in todoTasks via id
+    for(i = 0; i , todoTasks.length; i++){
+      console.log(currId);
+      console.log(todoTasks[i].id);
+      if(todoTasks[i].id == currId){
+        todoTasks.splice(i, 1);
+      }
+    }
+  }
+  else if(currView === 'COMPLETED'){
+    const currId = id;
+    completedTasks.splice(index, 1);
+    rerender(completedTasks);
 
-//If the checkbox is checked, strike-through list item
-// var list = document.querySelector('ul');
-// list.addEventListener('click', function(ev){
-// 	if(ev.target.name == 'LI'){
-// 		ev.target.classList.toggle('checked');
-// 	}
-// }, false);
+    //Look for the task to delete in todoTasks via id
+    for(i = 0; i , todoTasks.length; i++){
+      console.log(currId);
+      console.log(todoTasks[i].id);
+      if(todoTasks[i].id == currId){
+        todoTasks.splice(i, 1);
+      }
+    }
+  }
+  else if(currView === 'ALL'){
+    todoTasks.splice(index, 1);
+    rerender(todoTasks);
+  }
+}
 
+//Rerender the todo list
+function rerender(inputArray){
+  //Clear the current UL (gets rid of all current LI's)
+  todoContainer.innerHTML = '';
 
-//Tried to actually delete whole tree, b/c it shows when toggling show all
-// //Clicking on the close button hides the task
-// var close = document.getElementsByClassName("close");
-// for(i = 0; i < close.length; i++){
-// 	close[i].onclick = function (){
-// 		var div = this.parentElement;
-// 		var child = div.firstElementChild;
-// 		while(child){
-// 			div.deleteChild(child)
-// 			child = div.lastElementChild;
-// 		}
-// 		div.delete();
-// 	}
-// }
+  //Create a UL parent to append to
+  var listWrapperUL = document.createElement('UL');
 
-//Clicking on the close button hides the task
-var close = document.getElementsByClassName("close");
-for(i = 0; i < close.length; i++){
-	close[i].onclick = function (){
-		var div = close[i].parentElement;
-		console.log(div);
-		div.remove();
-	}
+  //Construct new todos for the list
+  inputArray.forEach(function (todoItem, index){
+
+    var taskName = todoItem.inputTask;
+
+    //Create LI element
+    var listEle = createLI();
+
+    //Create checkbox element
+    var checkboxEle = createCheckbox();
+
+    //If the checkbox was checked before a rerender, display it as checked
+    if(todoItem.checked){
+      checkboxEle.checked = true;
+    }
+    
+    //Sets it so that when the checkbox is checked or unchecked, changes the checked property accordingly
+    checkboxEle.addEventListener('change', function(event){
+      //If it was checked before, change it to unchecked
+      if(todoItem.checked){
+        console.log(todoItem.checked)
+        todoItem.checked = false;
+      }
+      //Otherwise, change it to checked
+      else{
+        todoItem.checked = true;
+      }
+      console.log(todoTasks);
+
+    });
+
+    //Create span element for task
+    var taskEle = createTask(taskName);
+    //Create delete button element
+    var deleteEle = createDelete();
+
+    //Sets it so that when the close button is clicked, triggers delete task method
+    deleteEle.addEventListener('click', function(event){
+      obliterateTask(index, todoItem.id);
+    });
+
+    listEle.appendChild(checkboxEle);
+    listEle.appendChild(taskEle);
+    listEle.appendChild(deleteEle);
+
+    //Append each list child to the UL parent
+    listWrapperUL.appendChild(listEle);
+
+  })
+  //Append the UL parent to the div container in the index.html
+  todoContainer.appendChild(listWrapperUL);
+}
+
+//Create an LI element
+function createLI(){
+  var listEle = document.createElement("LI", {class:'todo-item'});
+  return listEle;
+}
+
+//Create a checkbox element
+function createCheckbox(){
+  var checkboxEle = document.createElement("INPUT");
+  checkboxEle.setAttribute('type', 'checkbox');
+  checkboxEle.setAttribute('class', 'list-checkbox');
+  return checkboxEle;
+}
+
+//Create a label element for the task
+function createTask(taskItem){
+  var taskText1 = taskItem;
+  var taskEle = document.createElement("LABEL");
+  var taskText = document.createTextNode(taskItem);
+  taskEle.appendChild(taskText);
+  return taskEle;
+}
+
+//Create a delete button element
+function createDelete(){
+  var deleteEle = document.createElement("SPAN");
+  deleteEle.setAttribute('class', 'obliterate')
+  // deleteEle.setAttribute('onclick', 'removeTask()');
+  var closeChar = document.createTextNode('\u00D7');
+  deleteEle.appendChild(closeChar);
+  return deleteEle;
 }
 
 //Function for showing all the tasks via allButton
 function showAll(){
-	var x = document.getElementsByTagName("LI");
 
-	for(i = 0; i < x.length; i++){
-		if(x[i].style.display === "none"){
-			x[i].style.display = "block";
-		}
-		else{
-			//This enables toggle functionality
-			//x[i].style.display = "none";
-		}
-	}
+  currView = 'ALL';
+
+  rerender(todoTasks);
 }
 
-//When user checks checkbox, toggles a 'contentEditable' attribute in the LI element, true or false
-//This will be used to indicate whether the checkbox is checked for showing active and completed tasks
-//NO LONGER NEEDED b/c of idea 2 for show active tasks button
-// function toggleChecked(){
-// 	var x = document.getElementsByClassName("listcheckbox")
-
-// 	for(i = 0; i < x.length; i++){
-
-// 		if(x[i].checked){
-// 			var y = x[i].parentElement;
-// 			y.setAttribute('contentEditable', 'true');
-// 			// x.getAttribute('contentEditable') = 'true';
-// 		}	
-// 		//
-// 		else if(x[i].unchecked){
-// 		 	var y = x[i].parentElement;
-// 		 	y.setAttribute('contentEditable', 'false');
-// 		}		
-// 	}
-// }
-
-
-// //Function for showing all the active tasks via activeButton
-// function showActive(){
-// 	var x = document.getElementsByTagName("LI");
-
-// 	for(i = 0; i < x.length; i++){
-// 		if(x[i].getAttribute('contentEditable') === 'true'){
-// 			if(x[i].style.display === "none"){
-// 				x[i].style.display = "block";
-// 			}
-// 			else{
-// 				x[i].style.display = "none";					//Currently toggles because uncheck doesnt set contentEdited back to false
-// 			}			
-// 		}
-// 		else{
-
-// 		}
-
-// 	}
-// }
-
-//Second idea for showing all the active tasks via activeButton
+//Retrieve the active tasks from todoTasks and store it in a separate array, then display
 function showActive(){
 
-	var x = document.getElementsByClassName("listcheckbox")
+  currView = 'ACTIVE';
 
-	for(i = 0; i < x.length; i++){
+  activeTasks = [];
 
-		if(x[i].checked){
-			var y = x[i].parentElement;
-			y.style.display = "none";
-			
-		}	
-		
-		else{
-		 	var y = x[i].parentElement;
-		 	y.style.display = "block";
-		}		
-	}
+  todoTasks.forEach(function (todoItem, index){
+    //If the task is checked, add to activeTasks array
+    if(todoItem.checked === false){
+      activeTasks.push(todoItem);
+    }
+  });
+
+  rerender(activeTasks);
 }
 
-//Function for showing all the completed tasks via activeButton
+//Retrieve the completed tasks from todoTasks and store it in a separate array, then display
 function showDone(){
 
-	var x = document.getElementsByClassName("listcheckbox")
+  currView = 'COMPLETED';
 
-	for(i = 0; i < x.length; i++){
+  completedTasks = [];
 
+  todoTasks.forEach(function (todoItem, index){
+    //If the task is checked, add to activeTasks array
+    if(todoItem.checked){
+      completedTasks.push(todoItem);
+    }
+  });
 
-		if(x[i].checked){
-		 	var y = x[i].parentElement;
-		 	y.style.display = "block";
-		}
-
-		else{
-			var y = x[i].parentElement;
-			y.style.display = "none";
-			
-		}	
-	}
+  rerender(completedTasks);
 }
-
-
-
-
